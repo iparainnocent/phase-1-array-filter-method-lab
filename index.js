75f7e922412f6a4ca925983be51dfd45674eb949
iparainnocent <innocent.ipara@student.moringaschool.com>
2024-07-01 15:15:07 +0300

    add index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+// Function to find matching drivers
+function findMatching(drivers, test) {
+    return drivers.filter(driver => driver.toLowerCase() === test.toLowerCase());
+  }
+
+ 
+function fuzzyMatch(drivers, test) {
+  return drivers.filter(driver => driver.toLowerCase().startsWith(test.toLowerCase()));
+}
+
+// Function to match name property
+function matchName(driverObjects, test) {
+  return driverObjects.filter(driver => driver.name.toLowerCase() === test.toLowerCase());
+}
+
+
+
